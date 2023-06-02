41febe9623164f8724ff85cceaf9ed63a87f510f
NOVA52 <GamingCraft52@gmail.com>
2023-06-02 19:19:52 +0200

    implemented slack command + staff custom slug

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
-import pkg from 'pg';
-const { Client } = pkg;
+import pg from 'pg';
+const { Client } = pg;
 import express from "express";
 import bodyParser from "body-parser";
 import isBot from "isbot";
 import querystring from "querystring";
 import dotenv from "dotenv";
+import bolt from "@slack/bolt";
+const { App, LogLevel } = bolt;
+
 
 dotenv.config();
+
+const SlackApp = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  appToken: process.env.SLACK_APP_TOKEN,
+  socketMode: true,
+  logLevel: LogLevel.DEBUG,
+});
 
 const connectionString = process.env.POSTGRES_CONNECTION_STRING;
 const client = new Client({
@@ -26,7 +36,27 @@ app.listen(port, () => {
   console.log("Hack.af is up and running on port", port);
 });
 
-app.get("/ping", (req, res) => {
+SlackApp.command("/hack.af", async ({ command, ack, say }) => {
+  await ack();
+
+  const originalUrl = command.text.split(' ')[0];
+  let slug = Math.random().toString(36).substring(7);
+  const customSlug = command.text.split(' ')[1];
+
+  const isStaff = await isStaffMember(command.user_id);
+  if (isStaff && customSlug) {
+    slug = customSlug;
+  }
+
+  await client.query(
+    "INSERT INTO links (slug, destination) VALUES ($1, $2)",
+    [slug, originalUrl]
+  );
+
+  await say(`Your short URL: https://hack.af/${slug}`);
+});
+
+app.get("/ping", (_req, res) => {
   res.send("pong");
 });
 
@@ -69,19 +99,22 @@ app.get("/*", (req, res) => {
 
   lookup(decodeURI(slug)).then(
     (destination) => {
+      var fullUrl = destination.destination;
+      if (!/^https?:\/\//i.test(fullUrl)) {
+        fullUrl = 'http://' + fullUrl;
+      }
       var resultQuery = combineQueries(
-        querystring.parse(new URL(destination).search),
+        querystring.parse(new URL(fullUrl).search),
         query
       );
-      const parsedDestination = new URL(destination);
+      const parsedDestination = new URL(fullUrl);
       const finalURL = parsedDestination.origin + parsedDestination.pathname + resultQuery + parsedDestination.hash
-      console.log({ finalURL })
       res.redirect(307, finalURL)
     },
-    (error) => {
+    (_err) => {
       res.redirect(302, "https://hackclub.com/404");
     }
-  ).catch(() => {
+  ).catch((_err) => {
     res.redirect(302, "https://goo.gl/" + slug);
   });
 });
@@ -102,7 +135,7 @@ const lookup = async (slug) => {
   );
 
   if (res.rows.length > 0) {
-    return res.rows[0].destination;
+    return res.rows[0];
   } else {
     throw new Error('Slug not found');
   }
@@ -114,10 +147,10 @@ async function logAccess(ip, ua, slug, url) {
   const botUA = ["apex/ping/v1.0"];
   if (process.env.BOT_LOGGING === "off" && (isBot(ua) || botUA.includes(ua))) return;
 
-  let linkId;
+  let linkData;
   try {
-    linkId = await lookup(slug, true);
-    if (linkId === null) {
+    linkData = await lookup(slug, true);
+    if (linkData === null) {
       console.log("Slug not found, skipping logging");
       return;
     }
@@ -130,7 +163,7 @@ async function logAccess(ip, ua, slug, url) {
     client_ip: ip,
     user_agent: ua,
     bot: isBot(ua) || botUA.includes(ua),
-    slug: linkId,
+    slug: linkData.id,
     url: url,
   };
 
@@ -145,7 +178,7 @@ async function logAccess(ip, ua, slug, url) {
       data.slug,
       data.url,
     ],
-    (err, res) => {
+    (err, _res) => {
       if (err) {
         console.error(err);
       }
@@ -172,3 +205,24 @@ function forceHttps(req, res, next) {
   }
   next();
 }
+
+const isStaffMember = async (userId) => {
+  const params = {
+    usergroup: 'S0DJXPY14'
+  };
+
+  try {
+    const result = await SlackApp.client.usergroups.users.list(params);
+    return result.users.includes(userId);
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
+};
+
+
+
+(async () => {
+  await SlackApp.start();
+  console.log("Hack.af Slack is running!");
+})();
