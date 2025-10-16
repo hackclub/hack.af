95bd233930c37f59bc0a795fda9672d3743ed237
Sam Liu <sam@samliu.dev>
2025-10-16 17:44:13 -0400

    Fix formatting

--- app.js
+++ app.js
@@ -1181,7 +1181,7 @@ const isStaffMember = async (userId) => {
         'U07UBCSSQH3',  // leafd 
         'U075RTSLDQ8', // Angad
         'U078DFX40A2', //Emma
-        'U05JNJZJ0BS', // CAN
+        'U05JNJZJ0BS'  // CAN
     ]);
     return allowedUsers.has(userId)
 };
