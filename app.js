be10f8d71e48a4479c8fc0d30e6ee95a1f97fe3c
dhyan <87286496+dhyans26@users.noreply.github.com>
2026-07-23 14:28:06 -0400

    dhyan && jared (#174)

--- app.js
+++ app.js
@@ -1214,6 +1214,8 @@ const isStaffMember = async (userId) => {
         'U06U80G86H1', // Reem
         'U05EZRFKRV4', // Nathan
         "U082DPCGPST", // ascerton pixi the third (@ascpixi)
+        'U0824G9PTFE', // dhyan
+        'U07HEH4N8UV', // @jps
         "U093AJCBP0C", // Alfie
     ]);
     return allowedUsers.has(userId)
