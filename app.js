6d450280d04d3875afbc8b3e173a3543a58a99bb
Freddie Yershon <freddieayershon@gmail.com>
2026-08-28 21:25:17 +0100

    Add HCB redirect

--- app.js
+++ app.js
@@ -545,6 +545,10 @@ app.get("/gib/:org", (req, res) => {
     res.redirect(302, "https://hcb.hackclub.com/donations/start/" + req.params.org);
 });
 
+app.get("/hcb/:org", (req, res) => {
+    res.redirect(302, "https://hcb.hackclub.com/" + req.params.org);
+});
+
 app.get("/gh/:repo", (req, res) => {
     res.redirect(302, "https://github.com/hackclub/" + req.params.repo);
 });
